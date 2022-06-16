2e9c4d948e507ba582ae0b1963bdbe46608e4c4b
demola malomo <malomo.alaba@yahoo.com>
2022-06-16 16:52:01 +0100

    image cropping

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textPrimary}>Daniel James</Text>
+      <View>
+        {/* original */}
+        <Image
+          style={styles.galleryPrimary}
+          source={{
+            uri: 'https://res.cloudinary.com/dtgbzmpca/image/upload/v1655382189/Man_1.jpg',
+          }}
+        />
+        {/* circular crop */}
+        {/* <Image
+          style={styles.galleryPrimary}
+          source={{
+            uri: 'https://res.cloudinary.com/dtgbzmpca/image/upload/w_300,h_300,c_fill,ar_1:1,g_auto,r_max/v1655382189/Man_1.jpg',
+          }}
+        /> */}
+        {/* square crop */}
+        {/* <Image
+          style={styles.galleryPrimary}
+          source={{
+            uri: 'https://res.cloudinary.com/dtgbzmpca/image/upload/w_300,h_300,c_fill,ar_1:1,g_auto,r_20/v1655382189/Man_1.jpg',
+          }}
+        /> */}
+      </View>
     </View>
   );
 }
 
+const win = Dimensions.get('window');
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 40,
+    paddingHorizontal: 20,
+  },
+
+  textPrimary: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+
+  galleryPrimary: {
+    height: 400,
+    width: '100%',
+    resizeMode: 'cover',
+    margin: 2,
   },
 });
